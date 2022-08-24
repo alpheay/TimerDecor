aeee78864dc035487b029502be4032072d192c87
alpheay <nik.nandi.1@gmail.com>
2022-08-24 01:06:00 -0400

    a

--- timerdecor/__init__.py
+++ timerdecor/__init__.py
@@ -1,14 +1,12 @@
-from codecs import xmlcharrefreplace_errors
-from re import L, M
 import time
 from multiprocessing import Pool
-from tkinter import N
+
 def timer(func):
     def wrapper(*args, **kwargs):
         start = time.time()
         func(*args, **kwargs)
         end = time.time()
-        print(f"[Ran in {(end - start)}] s") 
+        print(f"[Ran in {(end - start)} s]") 
     return wrapper
 
 def tryexcept(func):
@@ -27,30 +25,3 @@ def perftimer(func):
         print(f"[Ran in {(end - start)} s]")
     return wrapper
 
-
-
-class MyClass:
-    x = None
-    y = None
-    z = None
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-    
-    def __str__(self):
-        return str((self.x, self.y, self.z))
-    
-    def __add__(self, other):
-        L = self.x + other.x
-        M = self.y + other.y
-        N = self.z + other.z
-        f = MyClass(L, M, N)
-        return f
-
-    def __
-
-
-y = MyClass(10, 10, "Hi")
-z = MyClass(10, 10, "Hi")
-print(y + z)
